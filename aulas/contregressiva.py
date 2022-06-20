0ee92d8a963ad7eb84fe7db84bec3d1ebcbd3f04
victor luiz <100322056+VICTORLUIZALMEIDAMEDEIROS@users.noreply.github.com>
2022-06-20 19:59:42 -0300

    victor luiz almeida medeiros revisão 20/06/2022

---
+++ aulas/contregressiva.py
@@ -0,0 +1,10 @@
+def contagemRegressiva(n=30):
+    while(n):
+        print(n)
+        n = n-1
+
+    if n == 0:
+          print('decolar')
+    else:
+          print(n)
+contagemRegressiva(n=30)
---
+++ aulas/ex2.py
@@ -0,0 +1,6 @@
+def contagemRegressiva(n=5):
+    while(n):
+        print(n)
+        n=n-1
+contagemRegressiva(5)
+print('decolar')
