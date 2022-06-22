71ebc70ba6ef5f7c1e8b4d92bcdc6914360661cf
profclovesrocha <clovesoficial@gmail.com>
2022-06-22 19:33:30 -0300

    Aula Revisão Final Mod. 1
    Aula remota de revisão final da disciplina de LPC 1.
    - Mais...
    - Mais coisa...
    - Exemplo de mais coisas...

---
+++ aulas/revisao/contexum.py
@@ -0,0 +1,12 @@
+#ETE PORTO DIGITAL
+#PROF. CLOVES
+#Operadores com Texto e Recursividade Lógica em Python 
+
+#Exemplo: 1
+
+def contRegressiva(n=30):
+  if n == 0:
+    print('Decolar!')
+  else:
+    print(n)
+contRegressiva(n=30)
---
+++ aulas/revisao/main.py
@@ -0,0 +1,16 @@
+#ETE PORTO DIGITAL
+#PROF. CLOVES
+#Operadores com Texto e Recursividade Lógica em Python 
+
+#Exemplo: 2 
+n = 100
+
+def contagemRegressiva(n):
+  while(n !=0):
+    print(n)
+    n = n-1
+    
+    if n == 0: 
+      print('decolar')
+      
+contagemRegressiva(n)
